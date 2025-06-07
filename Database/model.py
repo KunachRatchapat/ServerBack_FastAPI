8370e2fb276c5e7c201fd3a7c220f5ebe6a7d5e4
Tehdev <kunatratchapat555@gmail.com>
2025-06-07 16:29:36 +0700

    Schema No Sucess

--- Database/model.py
+++ Database/model.py
@@ -4,32 +4,39 @@ from datetime import datetime
 
 #Table User
 class Users (SQLModel , table = True):
-    id : int = Field(default =None, primary_key=True)
+    id : Optional[int] = Field(default =None, primary_key=True)
     email : str = Field()
     password : str = Field()
     name : str = Field(index = True)
     surname : str = Field()
-    createat : datetime = Field()
-    deleteat : datetime = Field()
-    updateat : datetime = Field()
     identification : str = Field()
+    createat : datetime = Field(default_factory=datetime.now)
+    deleteat : datetime = Field(default_factory=datetime.now)
+    updateat : datetime = Field(default_factory=datetime.now)
+    vegetable_id : int = Field(foreign_key="vegetable.id")  #relation with Vegetable
+    favorite_id : int = Field(foreign_key="favorite.id")    #relation with Favorite
+    
+    
     
 #Table Vegetable
 class Vegetable (SQLModel , table=True):
-    id : int = Field(default= None, primary_key=True)
+    id : Optional[int] = Field(default= None, primary_key=True)
     name : str = Field(index = True)
     description : str = Field()
     picture : str =  Field(index = True)
     createat : datetime = Field(default_factory=datetime.now)
     deleteat: datetime = Field(default_factory=datetime.now)
     updateat: datetime = Field(default_factory=datetime.now)
+    users_id : int = Field(foreign_key="user.id")   #relation with Users
+    favorite_id : int = Field(foreign_key="favorite.id") #relation with Favorite
+    
     
 #Table Favorite
 class Favorite (SQLModel , table = True):
     id : int = Field(default = None , primary_key = True)
-    user : Users
-    vegetables : Vegetable
-    createat : datetime = Field(defalt_factory =datetime.now)
+    users_id :int = Field(foreign_key="user.id") #relation with Users
+    vegetable_id : int = Field(foreign_key="vegetable.id") #relation with Vegetable
+    createat : datetime = Field(default_factory=datetime.now)
     deleteat : datetime = Field(default_factory=datetime.now)
     updateat : datetime = Field(default_factory=datetime.now)
     
