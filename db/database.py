d5e52866dfbdf3ec80ceccfec467a7185a9ce251
Tehdev <kunatratchapat555@gmail.com>
2025-06-09 00:21:38 +0700

    Fix CRUD No search

--- db/database.py
+++ db/database.py
@@ -1,37 +1,14 @@
-from typing import Union, Annotated
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-from fastapi import FastAPI, Depends
-from contextlib import contextmanager
-from datetime import datetime
+from sqlmodel import SQLModel, create_engine, Session
 
-# Create FastAPI instance
-app = FastAPI()
-
-# Variable database
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-# Sen to SQLite
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
-
-# Func CreateTable
 def create_db_and_tables():
+    from db.model import Vegetable  # หรือ import ทุกโมเดลที่ใช้
     SQLModel.metadata.create_all(engine)
 
-# ฟังก์ชัน get_session สำหรับ dependency injection
 def get_session():
     with Session(engine) as session:
         yield session
-
-# ใช้ Annotated สำหรับ dependency
-SessionDep = Annotated[Session, Depends(get_session)]
-
-#Create Table at Rundev
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
-
-
-
--- routers/vegetable.py
+++ routers/vegetable.py
@@ -1,19 +1,43 @@
-from typing import Union
-from fastapi import FastAPI , Request ,APIRouter
-   
-router = APIRouter()
+from fastapi import APIRouter, HTTPException, Depends, Query
+from typing import Annotated
+from sqlmodel import select, Session
+from db.model import Vegetable
+from db.database import get_session
 
-@router.post("/vegetable")
-async def add_vegetable():
-        return {"Hello Addvet"}
+router = APIRouter(prefix="/vegetables", tags=["Vegetables"]) 
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+#--Add Vegetable-- (For Dev)
+@router.post("/vegetable",response_model=Vegetable)
+def create_vegetable(vegetable:Vegetable , session: SessionDep):
+    session.add(vegetable)
+    session.commit()
+    session.refresh(vegetable)
+    return vegetable
+
+
+#--Show All Vegetable-- (Home)
+@router.get("/vegetables", response_model=list[Vegetable])
+def read_vegetables(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100
+):
+    vegetables = session.exec(select(Vegetable).offset(offset).limit(limit)).all()
+    return vegetables
+
+
+#--Delete Vegetable--
+@router.delete("/vegetable/{vegetable_id}")
+def delete_vegetable(vegetable_id: int , session: Session):
+    vegetable = (Vegetable ,vegetable_id)
+    if not vegetable :
+        raise HTTPException(status_code=404 ,detail= "Vegetable not found")
+    session.delete(vegetable)
+    session.commit()
+    return {"Success": True}
     
     
-@router.get("/vegetable")
-async def read_vegetables():
-    return {"Show Vet"}
-
-@router.get("vegetable/{vet_id}")
-async def read_vegetable():
-    return {"Show bobo"}
     
 
