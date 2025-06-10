13d9dce0325d26fa8b85ee2b0062b2746717efbe
Tehdev <kunatratchapat555@gmail.com>
2025-06-11 02:53:13 +0700

    fix docker yml No Success

--- db/database.py
+++ db/database.py
@@ -9,8 +9,8 @@ load_dotenv()
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT","5432")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
 # เชื่อมไปยัง Postgres ที่รันใน Docker
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
@@ -18,11 +18,18 @@ DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST
 #create DB
 engine = create_engine(DATABASE_URL, echo=True)
 
-
+#Create Table
 def create_db_and_tables():
-    from db.model import Vegetable, Users , Favorite , Fruit
-    SQLModel.metadata.create_all(engine)
+    try:
+        from db.model import Vegetable, Users , Favorite , Fruit
+        SQLModel.metadata.create_all(engine)
+        print("Create Table Success ")
+        
+    except Exception as e:
+        print("Eror create Table",str(e))
+        
     
 def get_session():
     with Session(engine) as session:
         yield session
+
