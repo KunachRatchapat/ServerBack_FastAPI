e0d4c9b23d22caa9c1f4d48a5235606041ef98f3
kunatratchapat555 <kunatratchapat555@gmail.com>
2025-08-25 17:30:56 +0700

    ✅ Success Repo:jwt, userss

--- repository/users.py
+++ repository/users.py
@@ -1,20 +1,10 @@
-from typing import Optional, TypeVar
-from sqlmodel import Session, SQLModel
-from datetime import datetime, timedelta
-from jose import jwt, JWTError
-import os
-from dotenv import load_dotenv
-
-# Load env
-load_dotenv()
-SECRET_KEY: str = os.getenv("SECRET_KEY", "default_secret")
-ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
-
-T = TypeVar("T", bound=SQLModel)
+from typing import Optional
+from sqlmodel import Session, select
+from db.models.user_model import Users
 
 class BaseRepo:
     @staticmethod
-    def insert(db: Session, model: T):
+    def insert(db: Session, model: Users) -> Users:
         db.add(model)
         db.commit()
         db.refresh(model)
@@ -22,21 +12,10 @@ class BaseRepo:
 
 class UsersRepo(BaseRepo):
     @staticmethod
-    def find_by_username(db: Session, model: type[T], username: str) -> Optional[T]:
-        return db.query(model).filter(model.username == username).first()
-
-class JWTRepo:
+    def find_by_email(db: Session, email: str) -> Optional[Users]:
+        return db.exec(select(Users).where(Users.email == email)).first()
+    
+    #---find Name---
     @staticmethod
-    def generate_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
-        to_encode = data.copy()
-        expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=15))
-        to_encode.update({"exp": expire})
-        return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-    @staticmethod
-    def decode_token(token: str) -> Optional[dict]:
-        try:
-            decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-            return decoded_token if decoded_token["exp"] >= datetime.utcnow().timestamp() else None
-        except JWTError:
-            return None
+    def find_by_name(db: Session, name: str) -> Optional[Users]:
+        return db.exec(select(Users).where(Users.name == name)).first()
